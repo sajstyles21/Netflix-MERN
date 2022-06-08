1ae0257badf0e34db3bdec2f3869ecdbf8e888cd
Suraj Sharma <sharmasuraj41@gmail.com>
2022-06-08 12:12:15 +0530

    changes

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 
-const BASE_URL = "https://netflix-backend-22.herokuapp.com/";
+//const BASE_URL = "https://netflix-backend-22.herokuapp.com/";
+const BASE_URL = "http://localhost:8800/";
 
 let user = JSON.parse(localStorage.getItem("user"));
 const TOKEN = user?.accessToken;
